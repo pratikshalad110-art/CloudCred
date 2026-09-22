b7c6d3735cc260c5f0de23c7c8db4a5cf74ecf95
Pratiksha <pratikshalad110@gmail.com>
2026-09-22 11:46:26 +0530

    Fix CORS and deployment

--- Backend/app.py
+++ Backend/app.py
@@ -29,7 +29,8 @@ CORS(
     supports_credentials=True,
     origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://cloudcred-1.onrender.com"
     ]
 )
 
@@ -870,11 +871,10 @@ def generate_qr():
     # =====================================================
     # VERIFICATION URL
     #
-    # LOCAL:
-    # http://127.0.0.1:5000
-    #
-    # RENDER:
-    # Set BASE_URL in Render environment variables.
+    # For Render:
+    # Set BASE_URL in Render environment variables
+    # to:
+    # https://cloudcred.onrender.com
     # =====================================================
 
     base_url = os.environ.get(
